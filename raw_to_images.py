0d158aabc1d0d05541ae2d76e19ae896a2d3c543
victor-yon <victor.yon@usherbrooke.ca>
2021-03-04 10:50:45 -0500

    Add raw data plotting and focus area

--- raw_to_images.py
+++ raw_to_images.py
@@ -1,13 +1,14 @@
-from typing import Iterable, List, Tuple
+from typing import Iterable, List, Optional, Tuple
 from zipfile import ZipFile
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas
+from matplotlib.pyplot import figure
 from scipy.interpolate import griddata
 
 
-def image_interpolation(diagram, step=0.001, method='nearest', filter_extreme=False):
+def image_interpolation(diagram, step=0.001, method='nearest', filter_extreme=False) -> Tuple:
     """
     Convert a set of irregular point into pixels using interpolation.
 
@@ -38,9 +39,50 @@ def image_interpolation(diagram, step=0.001, method='nearest', filter_extreme=Fa
     return x_i, y_i, grid
 
 
-def plot_image(x_i, y_i, pixels, image_name: str, annotations: Iterable[Tuple[List, List]] = None) -> None:
+def plot_raw(diagram, image_name: str, focus_area: Optional[Tuple] = None, grid_size: float = None) -> None:
+    """
+    Scatter plot of raw data points.
+
+    :param diagram: The diagram as a pandas dataframe. With columns x, y, z.
+    :param image_name: The image name, used in title.
+    :param focus_area: Optional coordinates to restrict the plotting area. A Tuple as (x_min, x_max, y_min, y_max).
+    :param grid_size: The size of the grid to plot, useful to compare illustrate the pixel interpolation.
+    """
+    figure(num=None, dpi=250)  # Increase image resolution to see the dots
+
+    # Draw the grid
+    # FIXME the grid lines doesn't match with pixels because they don't start at 0
+    if grid_size:
+        for i in np.arange(min(diagram.y), max(diagram.y), grid_size):
+            plt.axhline(i, linestyle='-', color='lightgrey', linewidth=1, zorder=0)
+        for i in np.arange(min(diagram.x), max(diagram.x), grid_size):
+            plt.axvline(i, linestyle='-', color='lightgrey', linewidth=1, zorder=0)
+
+    plt.scatter(diagram.x, diagram.y, c=diagram.z, cmap='copper', s=1, zorder=10)
+
+    plt.title(f'{image_name} - raw' + (f' (grid {grid_size})' if grid_size else ''))
+
+    # Select only a part of the plot
+    if focus_area:
+        plt.axis(focus_area)
+
+    plt.show(dpi=200)
+
+
+def plot_image(x_i, y_i, pixels, image_name: str, interpolation_method: str, pixel_size: float,
+               annotations: Iterable[Tuple[List, List]] = None,
+               focus_area: Optional[Tuple] = None) -> None:
     """
     Plot the interpolated image.
+
+    :param x_i: The x coordinates of the pixels (post interpolation)
+    :param y_i: The y coordinates of the pixels (post interpolation)
+    :param pixels: The list of pixels to plot
+    :param image_name: The name of the image, used for plot title
+    :param interpolation_method: The pixels interpolation method, used for plot title
+    :param pixel_size: The size of pixels, in voltage, used for plot title
+    :param annotations: The annotation to draw on top of the image
+    :param focus_area: Optional coordinates to restrict the plotting area. A Tuple as (x_min, x_max, y_min, y_max).
     """
 
     if annotations is not None:
@@ -50,15 +92,36 @@ def plot_image(x_i, y_i, pixels, image_name: str, annotations: Iterable[Tuple[Li
 
     plt.imshow(pixels, interpolation='none', cmap='copper',
                extent=[np.min(x_i), np.max(x_i), np.min(y_i), np.max(y_i)])
-    plt.title(f'{image_name} - interpolated')
+    plt.title(f'{image_name}\ninterpolated ({interpolation_method}) - pixel size {pixel_size}')
+
+    if focus_area:
+        plt.axis(focus_area)
+
     plt.show()
 
 
 if __name__ == '__main__':
+    pixel_size = 0.0025
+    interpolation_method = 'nearest'
+
     # Open the zip file and iterate over all csv files
     with ZipFile('data/raw_clean.zip', 'r') as zip_file:
         for diagram_files in zip_file.namelist():
             with zip_file.open(diagram_files) as diagram_file:
+                # Plot a specific area of the diagram
+                # focus_area = None
+                focus_area = (-0.460, -0.440, -0.65, -0.63)
+
+                # Load data
                 diagram = pandas.read_csv(diagram_file)
-                x_i, y_i, pixels = image_interpolation(diagram, filter_extreme=True)
-                plot_image(x_i, y_i, pixels, diagram_files)
+
+                # Plot raw points
+                plot_raw(diagram, diagram_files, focus_area, grid_size=None)
+
+                # Interpolate and plot image
+                x_i, y_i, pixels = image_interpolation(diagram,
+                                                       method=interpolation_method,
+                                                       step=pixel_size,
+                                                       filter_extreme=True)
+                plot_image(x_i, y_i, pixels, diagram_files, interpolation_method, pixel_size, focus_area=focus_area)
+                # break
