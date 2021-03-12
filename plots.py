db896183d231a02a4d47b3f27c0735a9bf797e57
victor-yon <victor.yon@usherbrooke.ca>
2021-03-12 18:28:43 -0500

    Load line annotations

--- plots.py
+++ plots.py
@@ -2,7 +2,7 @@ from typing import Iterable, Optional, Tuple
 
 import matplotlib.pyplot as plt
 import numpy as np
-from shapely.geometry import Polygon
+from shapely.geometry import LineString, Polygon
 
 REGION_SHORT = {
     '0_electron': '0',
@@ -44,7 +44,8 @@ def plot_raw(diagram, image_name: str, focus_area: Optional[Tuple] = None, grid_
 
 
 def plot_image(x_i, y_i, pixels, image_name: str, interpolation_method: str, pixel_size: float,
-               regions: Iterable[Tuple[str, Polygon]] = None, focus_area: Optional[Tuple] = None) -> None:
+               charge_regions: Iterable[Tuple[str, Polygon]] = None, transition_lines: Iterable[LineString] = None,
+               focus_area: Optional[Tuple] = None) -> None:
     """
     Plot the interpolated image.
 
@@ -54,19 +55,25 @@ def plot_image(x_i, y_i, pixels, image_name: str, interpolation_method: str, pix
     :param image_name: The name of the image, used for plot title
     :param interpolation_method: The pixels interpolation method, used for plot title
     :param pixel_size: The size of pixels, in voltage, used for plot title
-    :param regions: The charge region annotations to draw on top of the image
+    :param charge_regions: The charge region annotations to draw on top of the image
+    :param transition_lines: The transition line annotation to draw on top of the image
     :param focus_area: Optional coordinates to restrict the plotting area. A Tuple as (x_min, x_max, y_min, y_max).
     """
 
     plt.imshow(pixels, interpolation='none', cmap='copper',
                extent=[np.min(x_i), np.max(x_i), np.min(y_i), np.max(y_i)])
 
-    if regions is not None:
-        for i, (label, polygon) in enumerate(regions):
+    if charge_regions is not None:
+        for label, polygon in charge_regions:
             polygon_x, polygon_y = polygon.exterior.coords.xy
             plt.fill(polygon_x, polygon_y, 'b', alpha=.3, edgecolor='b', snap=True)
             label_x, label_y = list(polygon.centroid.coords)[0]
             plt.text(label_x, label_y, REGION_SHORT[label], ha="center", va="center", color='b')
+
+    if transition_lines is not None:
+        for line in transition_lines:
+            line_x, line_y = line.coords.xy
+            plt.plot(line_x, line_y, color='lime', alpha=.5)
 
     plt.title(f'{image_name}\ninterpolated ({interpolation_method}) - pixel size {round(pixel_size, 10) * 1_000}mV')
     plt.xlabel('Gate 1 (V)')
--- process_annotations.py
+++ process_annotations.py
@@ -4,7 +4,8 @@ from pathlib import Path
 from typing import List, Tuple
 from zipfile import ZipFile
 
-from shapely.geometry import Polygon
+import pandas as pd
+from shapely.geometry import LineString, Polygon
 
 from raw_to_images import load_interpolated_csv, plot_image
 
@@ -16,17 +17,45 @@ def clip(n, smallest, largest):
     return max(smallest, min(n, largest))
 
 
+def coord_to_volt(coord: List[float], min_coord: int, max_coord: int, value_start: float, value_step: float,
+                  snap: int = 1, is_y: bool = False) -> List[float]:
+    """
+    Convert some coordinates to volt value for a specific stability diagram.
+
+    :param coord: The list coordinates to convert
+    :param min_coord: The minimal valid value for the coordinate (before volt conversion)
+    :param max_coord: The maximal valid value for the coordinate (before volt conversion)
+    :param value_start: The voltage value of the 0 coordinate
+    :param value_step: The voltage difference between two coordinates (pixel size)
+    :param snap: The snap margin, every points near to image border at this distance will be rounded to the image border
+    (in number of pixels)
+    :param is_y: If true this is the y axis (to apply a rotation)
+    :return: The list of coordinates as gate voltage values
+    """
+    if is_y:
+        # Flip Y axis (I don't know why it's required)
+        coord = list(map(lambda t: max_coord - t, coord))
+
+    # Snap to border to avoid errors
+    coord = list(map(lambda t: clip(t, min_coord, max_coord), coord))
+
+    # Convert coordinates to actual voltage value
+    coord = list(map(lambda t: t * value_step + value_start, coord))
+
+    return coord
+
+
 def load_charge_annotations(annotations_json, image_name: str, x, y, snap: int = 1) -> List[Tuple[str, Polygon]]:
     """
     Load regions annotation for an image.
 
-    :param annotations_json: The json file containing all annotations.
+    :param annotations_json: The json structure containing all annotations
     :param image_name: The name of the image (should match with the name in the annotation file)
     :param x: The x axis of the diagram (in volt)
     :param y: The y axis of the diagram (in volt)
     :param snap: The snap margin, every points near to image border at this distance will be rounded to the image border
     (in number of pixels)
-    :return: The list of regions annotation for the image, as (label, list of x points, list of y points)
+    :return: The list of regions annotation for the image, as (label, shapely.geometry.Polygon)
     """
 
     if image_name not in annotations_json:
@@ -47,20 +76,12 @@ def load_charge_annotations(annotations_json, image_name: str, x, y, snap: int =
         y_r = region['shape_attributes']['all_points_y']
         label_r = region['region_attributes']['label']
 
+        x_r = coord_to_volt(x_r, min_x, max_x, x[0], step, snap)
+        y_r = coord_to_volt(y_r, min_y, max_y, y[0], step, snap, True)
+
         # Close regions
         x_r.append(x_r[-1])
         y_r.append(y_r[-1])
-
-        # Flip Y axis (I don't know why it's required)
-        y_r = list(map(lambda t: max_y - t, y_r))
-
-        # Snap to border to avoid errors
-        x_r = list(map(lambda t: clip(t, min_x, max_x), x_r))
-        y_r = list(map(lambda t: clip(t, min_y, max_y), y_r))
-
-        # Convert coordinates to actual values range
-        x_r = list(map(lambda t: t * step + x[0], x_r))
-        y_r = list(map(lambda t: t * step + y[0], y_r))
 
         polygon = Polygon(zip(x_r, y_r))
         regions.append((label_r, polygon))
@@ -68,10 +89,48 @@ def load_charge_annotations(annotations_json, image_name: str, x, y, snap: int =
     return regions
 
 
+def load_lines_annotations(lines_annotations_df, image_name: str, x, y, snap: int = 1):
+    """
+    Load transition line annotations for an image.
+
+    :param lines_annotations_df: The dataframe structure containing all annotations
+    :param image_name: The name of the image (should match with the name in the annotation file)
+    :param x: The x axis of the diagram (in volt)
+    :param y: The y axis of the diagram (in volt)
+    :param snap: The snap margin, every points near to image border at this distance will be rounded to the image border
+    (in number of pixels)
+    :return: The list of line annotation for the image, as shapely.geometry.LineString
+    """
+    current_file_lines = lines_annotations_df[lines_annotations_df['image_name'] == image_name]
+
+    # Define borders for snap
+    min_x, max_x = 0, len(x) - 1
+    min_y, max_y = 0, len(y) - 1
+    # Step (should be the same for every measurement)
+    step = x[1] - x[0]
+
+    lines = []
+    for _, l in current_file_lines.iterrows():
+        line_x = [l['x1'], l['x2']]
+        line_y = [l['y1'], l['y2']]
+
+        line_x = coord_to_volt(line_x, min_x, max_x, x[0], step, snap)
+        line_y = coord_to_volt(line_y, min_y, max_y, y[0], step, snap, True)
+
+        line = LineString(zip(line_x, line_y))
+        lines.append(line)
+
+    return lines
+
+
 def main():
     # Open the json file that can contain annotations for every diagrams
     with open(Path(DATA_DIR, 'charge_area.json'), 'r') as annotations_file:
-        annotations_json = json.load(annotations_file)
+        charge_annotations_json = json.load(annotations_file)
+
+    lines_annotations_df = pd.read_csv(Path(DATA_DIR, 'transition_lines.csv'),
+                                       usecols=[1, 2, 3, 4, 5],
+                                       names=['x1', 'y1', 'x2', 'y2', 'image_name'])
 
     # Open the zip file and iterate over all csv files
     with ZipFile(Path(DATA_DIR, 'interpolated_csv.zip'), 'r') as zip_file:
@@ -81,9 +140,11 @@ def main():
                 # Load values from CSV file
                 x, y, values = load_interpolated_csv(gzip.open(diagram_file))
 
-                diagram_annotations = load_charge_annotations(annotations_json, f'{file_basename}.png', x, y, snap=1)
+                # Load annotation and convert the coordinates to volt
+                charge_regions = load_charge_annotations(charge_annotations_json, f'{file_basename}.png', x, y, snap=1)
+                transition_lines = load_lines_annotations(lines_annotations_df, f'{file_basename}.png', x, y, snap=1)
 
-                plot_image(x, y, values, file_basename, 'nearest', x[1] - x[0], diagram_annotations)
+                plot_image(x, y, values, file_basename, 'nearest', x[1] - x[0], charge_regions, transition_lines)
                 break
 
 
