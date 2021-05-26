0f7066c174344b2189d5c31048a43436583a742e
victor-yon <victor.yon@usherbrooke.ca>
2021-05-26 18:30:37 -0400

    Adapt import label from Labelbox json format

--- process_annotations.py
+++ process_annotations.py
@@ -1,10 +1,9 @@
 import gzip
 import json
 from pathlib import Path
-from typing import List, Tuple
+from typing import Iterable, List, Tuple
 from zipfile import ZipFile
 
-import pandas as pd
 from shapely.geometry import LineString, Polygon
 
 from plots import plot_image
@@ -18,7 +17,7 @@ def clip(n, smallest, largest):
     return max(smallest, min(n, largest))
 
 
-def coord_to_volt(coord: List[float], min_coord: int, max_coord: int, value_start: float, value_step: float,
+def coord_to_volt(coord: Iterable[float], min_coord: int, max_coord: int, value_start: float, value_step: float,
                   snap: int = 1, is_y: bool = False) -> List[float]:
     """
     Convert some coordinates to volt value for a specific stability diagram.
@@ -46,7 +45,7 @@ def coord_to_volt(coord: List[float], min_coord: int, max_coord: int, value_star
     return coord
 
 
-def load_charge_annotations(annotations_json, image_name: str, x, y, snap: int = 1) -> List[Tuple[str, Polygon]]:
+def load_charge_annotations(annotations_json, x, y, snap: int = 1) -> List[Tuple[str, Polygon]]:
     """
     Load regions annotation for an image.
 
@@ -90,19 +89,17 @@ def load_charge_annotations(annotations_json, image_name: str, x, y, snap: int =
     return regions
 
 
-def load_lines_annotations(lines_annotations_df, image_name: str, x, y, snap: int = 1):
+def load_lines_annotations(lines: Iterable, x, y, snap: int = 1):
     """
     Load transition line annotations for an image.
 
-    :param lines_annotations_df: The dataframe structure containing all annotations
-    :param image_name: The name of the image (should match with the name in the annotation file)
+    :param lines: List of line label as json object (from Labelbox export)
     :param x: The x axis of the diagram (in volt)
     :param y: The y axis of the diagram (in volt)
     :param snap: The snap margin, every points near to image border at this distance will be rounded to the image border
     (in number of pixels)
     :return: The list of line annotation for the image, as shapely.geometry.LineString
     """
-    current_file_lines = lines_annotations_df[lines_annotations_df['image_name'] == image_name]
 
     # Define borders for snap
     min_x, max_x = 0, len(x) - 1
@@ -110,42 +107,46 @@ def load_lines_annotations(lines_annotations_df, image_name: str, x, y, snap: in
     # Step (should be the same for every measurement)
     step = x[1] - x[0]
 
-    lines = []
-    for _, l in current_file_lines.iterrows():
-        line_x = [l['x1'], l['x2']]
-        line_y = [l['y1'], l['y2']]
-
-        line_x = coord_to_volt(line_x, min_x, max_x, x[0], step, snap)
-        line_y = coord_to_volt(line_y, min_y, max_y, y[0], step, snap, True)
+    processed_lines = []
+    for line in lines:
+        line_x = coord_to_volt((p['x'] for p in line['line']), min_x, max_x, x[0], step, snap)
+        line_y = coord_to_volt((p['y'] for p in line['line']), min_y, max_y, y[0], step, snap, True)
 
         line = LineString(zip(line_x, line_y))
-        lines.append(line)
+        processed_lines.append(line)
 
-    return lines
+    return processed_lines
 
 
 def main():
     # Open the json file that can contain annotations for every diagrams
-    with open(Path(DATA_DIR, 'charge_area.json'), 'r') as annotations_file:
-        charge_annotations_json = json.load(annotations_file)
+    with open(Path(DATA_DIR, 'labels.json'), 'r') as annotations_file:
+        labels_json = json.load(annotations_file)
 
-    lines_annotations_df = pd.read_csv(Path(DATA_DIR, 'transition_lines.csv'),
-                                       usecols=[1, 2, 3, 4, 5],
-                                       names=['x1', 'y1', 'x2', 'y2', 'image_name'])
+    print(f'{len(labels_json)} labeled diagrams found')
+    labels = {obj['External ID']: obj for obj in labels_json}
 
     # Open the zip file and iterate over all csv files
     with ZipFile(Path(DATA_DIR, 'interpolated_csv.zip'), 'r') as zip_file:
         for diagram_name in zip_file.namelist():
             file_basename = Path(diagram_name).stem  # Remove extension
+
+            if f'{file_basename}.png' not in labels:
+                raise RuntimeError(f'No label found for {file_basename}')
+
             with zip_file.open(diagram_name) as diagram_file:
                 # Load values from CSV file
                 x, y, values = load_interpolated_csv(gzip.open(diagram_file))
 
-                # Load annotation and convert the coordinates to volt
-                charge_regions = load_charge_annotations(charge_annotations_json, f'{file_basename}.png', x, y, snap=1)
-                transition_lines = load_lines_annotations(lines_annotations_df, f'{file_basename}.png', x, y, snap=1)
+                current_labels = labels[f'{file_basename}.png']['Label']['objects']
 
-                plot_image(x, y, values, file_basename, 'nearest', x[1] - x[0], charge_regions, transition_lines)
+                # Load annotation and convert the coordinates to volt
+                transition_lines = load_lines_annotations(filter(lambda x: x['title'] == 'line', current_labels), x, y,
+                                                          snap=1)
+                # charge_regions = load_charge_annotations(filter(lambda x: x.title != 'line', current_labels), x, y, snap=1)
+
+                plot_image(x, y, values, file_basename, 'nearest', x[1] - x[0], charge_regions=None,
+                           transition_lines=transition_lines)
 
 
 if __name__ == '__main__':
